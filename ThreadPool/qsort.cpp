2fd4ced5749818048f6b512022a8b3282eedb17f
krylovgeorgii <krylov.georgii@gmail.com>
2018-02-18 01:52:13 +0300

    speed test finished (not review)

--- ThreadPool/qsort.cpp
+++ ThreadPool/qsort.cpp
@@ -55,13 +55,28 @@ Iter partQSort(Iter first, Iter last, int dis) {
 	return ++left;
 }
 
-
+template<class Iter>
 class QSortThreadPool {
 public:
 	QSortThreadPool(size_t num) : th(num) { }
 
-	template<class Iter>
 	void operator() (Iter first, Iter last) {
+		qSortTh(first, last);
+
+		while (th.getNumWorkThreads() != 0) {
+			using namespace std::chrono_literals;
+			std::this_thread::sleep_for(10ms);
+		}
+	}
+
+	void resize(size_t num) {
+		th.resize(num);
+	}
+
+private:
+	threadpool::ThreadPool th;
+
+	void qSortTh (Iter first, Iter last) {
 		auto dis = distance(first, last - 1);
 		if (dis > 0) {
 			auto border = partQSort(first, last, dis);
@@ -69,16 +84,18 @@ public:
 			bool dis1 = distance(first, border - 1) > 3;
 			bool dis2 = distance(border, last - 1) > 3;
 
+			auto func = [this](Iter f, Iter l) { qSortTh(f, l); };
+
 			if (dis1 && dis2) {
-				th.addTask(quickSort<Iter>, first, border);
-				th.addTask(quickSort<Iter>, border, last);
+				th.addTask(func, first, border);
+				th.addTask(func, border, last);
 			}
 			else if (dis1) {
-				th.addTask(quickSort<Iter>, first, border);
+				th.addTask(func, first, border);
 				std::sort(border, last);
 			}
 			else if (dis2) {
-				th.addTask(quickSort<Iter>, border, last);
+				th.addTask(func, border, last);
 				std::sort(first, border);
 			}
 			else {
@@ -87,8 +104,6 @@ public:
 			}
 		}
 	}
-
-	threadpool::ThreadPool th;
 };
 
 template<class Iter>
@@ -103,17 +118,12 @@ void quickSort(Iter first, Iter last) {
 }
 
 template<class Func, class Arr>
-void sortFunc(Func&& sort, Arr&& arr) {
-	sort(arr.begin(), arr.end());
-}
-
-template<class Func, class Arr>
 auto someSort(Func&& sort, Arr arr) {
 	std::vector<int> arrCopy(arr->size());
 	std::copy(arr->begin(), arr->end(), arrCopy.begin());
 
 	auto startTime = std::chrono::steady_clock::now();
-	sortFunc(sort, arrCopy);
+	sort(arrCopy.begin(), arrCopy.end());
 	auto finishTime = std::chrono::steady_clock::now();
 
 	return std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
@@ -122,36 +132,23 @@ auto someSort(Func&& sort, Arr arr) {
 void f(size_t sizeArr) {
 	using arrType = std::vector<int>;
 	auto arr = std::make_shared<arrType>(sizeArr);
+
 	std::srand(unsigned(std::time(0)));
 	for (auto&& t : *arr) {
 		t = rand() % 10;
 	}
 
-
-	auto getTime_QSortThreadPool = [arr] {
-		std::vector<int> arrCopy(arr->size());
-		std::copy(arr->begin(), arr->end(), arrCopy.begin());
-		QSortThreadPool qsth(4);
-
-		auto startTime = std::chrono::steady_clock::now();
-		sortFunc(qsth, arrCopy);
-		
-		while (qsth.th.getNumWorkThreads() != 0) {
-			using namespace std::chrono_literals;
-			std::this_thread::sleep_for(10ms);
-		}
-		auto finishTime = std::chrono::steady_clock::now();
-
-		return std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
-	};
-
 	std::cout << "std::sort " << someSort(std::sort<arrType::iterator>, arr) << std::endl;
-	std::cout << "QSortThreadPool " << getTime_QSortThreadPool() << std::endl;
 	std::cout << "quickSort " << someSort(quickSort<arrType::iterator>, arr) << std::endl;
+
+	QSortThreadPool<arrType::iterator> qsth(16);
+	using namespace std::chrono_literals;
+	std::this_thread::sleep_for(100ms);
+	std::cout << "QSortThreadPool " << someSort(qsth, arr) << std::endl;
 }
 
 int main() {
-	f(100'000);
+	f(50'000);
 
 	char c;
 	std::cin >> c;
